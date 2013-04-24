3ed88ac6bc7744e1bbce729c153104a979c71597
Sean B. Palmer <sean@miscoranda.com>
2013-04-24 16:04:55 +0100

    Published 0.2.002

--- saxo.py
+++ saxo.py
@@ -1,7 +1,7 @@
 # Copyright 2013, Sean B. Palmer
 # Source: http://inamidst.com/saxo/
 
-version = "0.2.001"
+version = "0.2.002"
 # WARNING: If updating anything before this message,
 # change the offset in setup.py
 
