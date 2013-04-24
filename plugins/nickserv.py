478b5cd4c170456aa075041e8687a81c16722692
Sean B. Palmer <sean@miscoranda.com>
2013-04-24 16:20:21 +0100

    Updated the NickServ registration plugin

--- plugins/nickserv.py
+++ plugins/nickserv.py
@@ -3,10 +3,11 @@
 
 import saxo
 
-@saxo.event("001")
+@saxo.event("251")
 def nickserv(irc):
-    if "nickserv" in irc.config:
-        # Works on freenode, not sure about other servers. Easy to modify
-        nick = irc.config["nick"]
-        password = irc.config["plugins"]["nickserv"]
-        irc.msg("NickServ", "IDENTIFY %s %s" % (nick, password))
+    if "plugins" in irc.config:
+        if "nickserv" in irc.config["plugins"]:
+            # Works on freenode, not sure about other servers. Easy to modify
+            nick = irc.config["nick"]
+            password = irc.config["plugins"]["nickserv"]
+            irc.msg("NickServ", "IDENTIFY %s %s" % (nick, password))
