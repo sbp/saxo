2ef70790d0c552794567e3fc78b20dd5d341efa1
Sean B. Palmer <sean@miscoranda.com>
2013-12-07 16:41:41 +0000

    Allow owners to use nickname only if CAP identified

--- plugins/admin.py
+++ plugins/admin.py
@@ -3,53 +3,56 @@
 
 import saxo
 
-@saxo.command("join")
-def join(irc):
+def owner(irc):
     if "owner" in irc.config:
         if irc.prefix == irc.config["owner"]:
-            if irc.arg.startswith("#"):
-                irc.client("join", irc.arg)
-                irc.say("Joining %s" % irc.arg)
+            return True
+        if irc.identified:
+            owner_nick = irc.config["owner"].split("!", 1)[0]
+            if irc.nick == owner_nick:
+                return True
+    return False
+
+@saxo.command("join")
+def join(irc):
+    if owner(irc):
+        if irc.arg.startswith("#"):
+            irc.client("join", irc.arg)
+            irc.say("Joining %s" % irc.arg)
 
 @saxo.command("leave")
 def leave(irc):
-    if "owner" in irc.config:
-        if irc.prefix == irc.config["owner"]:
-            if irc.arg.startswith("#"):
-                irc.send("PART", irc.arg)
-                irc.say("Leaving %s" % irc.arg)
+    if owner(irc):
+        if irc.arg.startswith("#"):
+            irc.send("PART", irc.arg)
+            irc.say("Leaving %s" % irc.arg)
 
 @saxo.command("part")
 def part(irc):
-    if "owner" in irc.config:
-        if irc.prefix == irc.config["owner"]:
-            if irc.arg.startswith("#"):
-                irc.client("part", irc.arg)
-                irc.say("Parting %s" % irc.arg)
+    if owner(irc):
+        if irc.arg.startswith("#"):
+            irc.client("part", irc.arg)
+            irc.say("Parting %s" % irc.arg)
 
 @saxo.command("prefix")
 def prefix(irc):
-    if "owner" in irc.config:
-        if irc.prefix == irc.config["owner"]:
-            irc.client("prefix", irc.arg)
-            irc.say("Setting prefix to %r" % irc.arg)
+    if owner(irc):
+        irc.client("prefix", irc.arg)
+        irc.say("Setting prefix to %r" % irc.arg)
 
 @saxo.command("quit")
 def quit(irc):
-    if "owner" in irc.config:
-        if irc.prefix == irc.config["owner"]:
-            irc.client("quit")
+    if owner(irc):
+        irc.client("quit")
 
 @saxo.command("reload")
 def reload(irc):
-    if "owner" in irc.config:
-        if irc.prefix == irc.config["owner"]:
-            irc.client("reload", irc.sender)
+    if owner(irc):
+        irc.client("reload", irc.sender)
 
 @saxo.command("visit")
 def visit(irc):
-    if "owner" in irc.config:
-        if irc.prefix == irc.config["owner"]:
-            if irc.arg.startswith("#"):
-                irc.send("JOIN", irc.arg)
-                irc.say("Visiting %s" % irc.arg)
+    if owner(irc):
+        if irc.arg.startswith("#"):
+            irc.send("JOIN", irc.arg)
+            irc.say("Visiting %s" % irc.arg)
