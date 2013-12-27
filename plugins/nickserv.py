e9cb6f8f47aeb3e464c6c66cb02c10b59b9d5ee1
Sean B. Palmer <sean@miscoranda.com>
2013-12-27 20:35:05 +0000

    Fix NickServ reconnection bug

--- plugins/nickserv.py
+++ plugins/nickserv.py
@@ -4,7 +4,16 @@
 import saxo
 
 @saxo.event("251")
-def nickserv(irc):
+def nickserv251(irc):
+    if "plugins" in irc.config:
+        if "nickserv" in irc.config["plugins"]:
+            # Works on freenode, not sure about other servers. Easy to modify
+            nick = irc.config["nick"]
+            password = irc.config["plugins"]["nickserv"]
+            irc.msg("NickServ", "IDENTIFY %s %s" % (nick, password))
+
+@saxo.event("451")
+def nickserv451(irc):
     if "plugins" in irc.config:
         if "nickserv" in irc.config["plugins"]:
             # Works on freenode, not sure about other servers. Easy to modify
